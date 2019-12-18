a8b05695d4dc8bc11127bf8c4a61635bc907e736
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-18 15:35:08 +0530

    Update app.js

--- tests/mysql-test/app.js
+++ tests/mysql-test/app.js
@@ -2,12 +2,21 @@
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'mysql',
-  user: 'biplab',
-  password: 'biplab@2019',
+  host: 'custom-mysql.gamification.svc.cluster.local',
+  user: 'xxuser',
+  password: 'welcome1',
   database: 'sampledb'
 });
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
+connection.query('SELECT SYSDATE()', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+connection.end();

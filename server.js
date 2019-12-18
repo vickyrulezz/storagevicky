933542c635dd0eb084acb2273a7a1e44886d5257
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-18 18:21:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,64 +1,55 @@
-//app.js
+// Demo server app - developed for Gamification demo .... 
+// This will expose some sample APIs via REST to call product database 
 
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-const request = require('request');
-    
-Object.assign=require('object-assign')
+// import express module - web app server
+// import body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body as something easier to interact with
+// import mysql modeule to connect to mysql
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require("mysql");
 
+// set port
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+// we will use JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+	
 
+// default route
 app.get('/', function (req, res) {
-  res.render('index.html');
-})
-
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'custom-mysql.gamification.svc.cluster.local',
-  user: 'xxuser',
-  password: 'welcome1',
-  database: 'sampledb'
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
+  res.render('index.html',{error : null, message :'This is the home index page'});
 });
 
-let commodity_name = process.argv[2]; // pass argument to query
-let sql = `select XXPC.COMMODITY_NAME PRODUCT_TYPE, XXSKU.ITEM_NUMBER SKU, XXPS.BRAND ,XXSKU.DESCRIPTION,XXSKU.LONG_DESCRIPTION, 
-XXPR.LIST_PRICE,XXSKU.SKU_ATTRIBUTE_VALUE1 SIZE,XXSKU.SKU_ATTRIBUTE_VALUE2 COLOR,XXPR.IN_STOCK from 
-XXIBM_PRODUCT_SKU XXSKU,
-XXIBM_PRODUCT_PRICING XXPR,
-XXIBM_PRODUCT_STYLE XXPS,
-XXIBM_PRODUCT_CATALOGUE XXPC
-where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER
-and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER
-AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY
-AND XXPC.COMMODITY_NAME = '` + commodity_name +`'` ;
 
-console.log(sql);
+//mysql configuration - Gamification project mysql connect details
+var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'custom-mysql.gamification.svc.cluster.local';
+var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
+var mysqlUser = 'xxuser'; 
+var mysqlPass = 'welcome1';
 
-connection.query(sql, function(err, results, fields) {
-  if (!err)
-    //console.log('The solution is: ', results);
-	 // if there is no error, you have the result
-		// iterate for all the rows in result
-		Object.keys(results).forEach(function(key) {
-		  var row = results[key];
-		  console.log(row.PRODUCT_TYPE +`|`+ row.SKU +`|`+ row.BRAND +`|`+ row.DESCRIPTION +`|`+ row.LONG_DESCRIPTION +`|`+ row.LIST_PRICE
-			+`|`+ row.SIZE +`|`+ row.COLOR +`|`+ row.IN_STOCK) 
-	});
-  else
-    console.log('Error while performing Query.');
+// we will use sampledb as database
+var mysqlDb = 'sampledb';
+
+//form the connection string to connect to mysql - you can connect directly too 
+var mysqlString = 'mysql://' + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
+console.log(mysqlString);
+
+
+//connect to mysql/sampledb database
+var mysqlClient = mysql.createConnection(mysqlString);
+mysqlClient.connect(function (err) {
+    if (err) console.log(err);
 });
 
-connection.end();
+
+
+// set port
+app.listen(port, ip);
+
+module.exports = app;

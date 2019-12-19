892d78eb719a4bfaa3424e251dac3196cc2dcf63
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-19 18:12:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 
 var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+    app     = express();
     
-Object.assign=require('object-assign')
-
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
 
 app.get('/', function (req, res) {
       res.render('index.html');

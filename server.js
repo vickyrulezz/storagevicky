75286fdca738aa9ba2f587149687a80823b4e900
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-19 18:34:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var express = require('express'),
-    app     = express();
-    
-Object.assign=require('object-assign')
+var http = require('http');
+var fs = require('fs');
 
-app.engine('html', require('ejs').renderFile);
-
-app.get('/', function (req, res) {
-      res.render('index.html');
+var server = http.createServer(function(req, resp){
+  // Print the name of the file for which request is made.
+  console.log("Request for demo file received.");
+  fs.readFile("views/index.html",function(error, data){
+    if (error) {
+      resp.writeHead(404);
+      resp.write('Contents you are looking for-not found');
+      resp.end();
+    }  else {
+      resp.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      resp.write(data.toString());
+      resp.end();
+    }
+  });
 });
+
+server.listen(8081, '127.0.0.1');

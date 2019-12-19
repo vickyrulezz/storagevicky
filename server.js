03576d1458147ab3170330e5df6f9c33813522a2
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-19 17:04:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-	
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-	;
-	
-app.get('/', function (req, res) {
-res.render('index.html');
-});
-
-
-	
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+let http = require('http');
+let fs = require('fs');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(8000);

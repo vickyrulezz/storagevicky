63818b3ab18c7cbcf81251e2cfa4b75fcc4adbfe
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-19 16:50:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+	
+Object.assign=require('object-assign')
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+	;
+	
+app.get('/', function (req, res) {
+res.render('index');
 });
+
+
+	
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;

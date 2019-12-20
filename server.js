302942448e2d3d434b248f7d2e1d51241886f7e8
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-20 17:19:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
+
+var path = require('path');
+const VIEWS = path.join(__dirname, 'views');
 	
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
@@ -9,11 +12,22 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     
 Object.assign=require('object-assign')
 
+app.set('views', VIEWS);
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', function (req, res) {
-      res.render('index.html');
+     res.render('index.html', { root : VIEWS });
+});
+
+app.get("/contact", function (req, res) {
+  res.sendFile('contact.html', { root : VIEWS });
+ });
+
+app.get("/about", function (req, res) {
+  res.sendFile('about.html', { root : VIEWS });
 });
 
 app.listen(port, ip);

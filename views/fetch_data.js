6187b668877f36ec943176a65d6bbef5068ed177
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-24 11:50:27 +0530

    Update fetch_data.js

--- views/fetch_data.js
+++ views/fetch_data.js
@@ -29,7 +29,7 @@ const pool = mysql.createPool({
 	console.log(pool);
 
 //html string that will be send to browser
-var reo ='<html><head><title>Node.js MySQL Select</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
+var resulthtml ='<html><head><title>Node.js MySQL Select</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
 
 let sql ='select XXSKU.ITEM_NUMBER SKU, XXSKU.DESCRIPTION,XXPR.LIST_PRICE from XXIBM_PRODUCT_SKU XXSKU,XXIBM_PRODUCT_PRICING XXPR,XXIBM_PRODUCT_STYLE XXPS,XXIBM_PRODUCT_CATALOGUE XXPC where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY';
 
@@ -57,12 +57,20 @@ pool.getConnection((err, con)=>{
 
       con.release(); //Done with mysql connection
 
-      return cb(table);
+	resulthtml = resulthtml.replace('{${table}}', table);
+      //return cb(table);
+	    return cb(resulthtml);
     });
   });
 }
 
+module.exports = {
+	setResHtml : setResHtml
+}
+	
+
 //module.exports = async () 
+/*
 module.exports = function extract_data()
 {
 	console.log("We are in module exports !!!");
@@ -75,22 +83,5 @@ module.exports = function extract_data()
 
         return extracted_data; 
 }
-
-
-/*
-//create the server for browser access
-const server = http.createServer((req, res)=>{
-  setResHtml(sql, resql=>{
-    reo = reo.replace('{${table}}', resql);
-    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    res.write(reo, 'utf-8');
-    res.end();
-  });
-});
 */
-
-//server.listen(8080, ()=>{
-//server.listen(port, ()=>{
-  //console.log('Server running at //localhost:8080/');
-//});
 

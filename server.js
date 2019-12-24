a65fdba4f246d8230e11c0f2669c2e9918c95428
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-24 17:15:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ var fetch_data_sql = require('./views/test_sql.js');
 fetch_data_sql.extract_data();
 //console.log(fetch_data_sql);
 //create the server for browser access
+
 /*
 const server = http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
@@ -78,7 +79,6 @@ const server = http.createServer((req, res)=>{
 	});
 });
 */
-
 // Port Listen
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);

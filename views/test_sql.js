f362560f286ce6cf8015cd8f3c9e47c7318a3822
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-24 13:47:52 +0530

    Update test_sql.js

--- views/test_sql.js
+++ views/test_sql.js
@@ -8,6 +8,9 @@ const connection = mysql.createConnection({
   database: 'sampledb'
 });
 
+//html string that will be send to browser
+var resulthtml ='<html><head><title>Kool App - Andromeda Product Page</title></head><body><h1>Product Details</h1>{${table}}</body></html>';
+
 let commodity_name = process.argv[2]; // pass argument to query
 let sql = `select XXPC.COMMODITY_NAME PRODUCT_TYPE, XXSKU.ITEM_NUMBER SKU, XXPS.BRAND ,XXSKU.DESCRIPTION,XXSKU.LONG_DESCRIPTION, 
 XXPR.LIST_PRICE,XXSKU.SKU_ATTRIBUTE_VALUE1 SIZE,XXSKU.SKU_ATTRIBUTE_VALUE2 COLOR,XXPR.IN_STOCK from 
@@ -17,7 +20,7 @@ XXIBM_PRODUCT_STYLE XXPS,
 XXIBM_PRODUCT_CATALOGUE XXPC
 where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER
 and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER
-AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY`;
+AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY`
 //AND XXPC.COMMODITY_NAME = '` + commodity_name +`'` ;
 
 console.log(sql);
@@ -28,27 +31,47 @@ var extract_data;
 
 module.exports = {
 	
-	extract_data: function(){
+	setResHtml: 
+	function (sql, cb){
+		console.log("We are in setResHtml");
 		connection.connect((err) => {
-			  if (err) throw err;
-			  console.log('Connected!');
-			});
+		  if (err) throw err;
+		  console.log('Connected!');
+		});
+		
 		connection.query(sql, function(err, results, fields) {
 		  if (!err)
 			//console.log('The solution is: ', results);
 			 // if there is no error, you have the result
 				// iterate for all the rows in result
+				console.log("Executing query !!!");
+				
 				Object.keys(results).forEach(function(key) {
 				  var row = results[key];
 				  console.log(row.PRODUCT_TYPE +`|`+ row.SKU +`|`+ row.BRAND +`|`+ row.DESCRIPTION +`|`+ row.LONG_DESCRIPTION +`|`+ row.LIST_PRICE
 					+`|`+ row.SIZE +`|`+ row.COLOR +`|`+ row.IN_STOCK) 
-			});
+					});
+				for(var i=0; i<res.length; i++){
+					table +='<tr><td>'+ (i+1) +'</td><td>'+ res[i].PRODUCT_TYPE +'</td><td>'+ res[i].SKU +'</td><td>'+ res[i].BRAND +'</td><td>'+ res[i].DESCRIPTION +'</td><td>'+ res[i].LONG_DESCRIPTION +'</td><td>'+ res[i].LIST_PRICE +'</td><td>'+ res[i].SIZE +'</td><td>'+ res[i].COLOR+'</td><td>'+ res[i].IN_STOCK +'</td></tr>';
+					}
+					table ='<table border="1"><tr><th>Sr No.</th><th>PRODUCT_TYPE</th><th>SKU</th><th>BRAND</th><th>DESCRIPTION</th><th>LONG_DESCRIPTION</th><th>LIST_PRICE</th><th>SIZE</th><th>COLOR</th><th>IN_STOCK</th></tr>'+ table +'</table>';
+					
+					resulthtml = resulthtml.replace('{${table}}', table);
+					return cb(resulthtml);
+		
+					connection.end();
+		  
 		  else
 			console.log('Error while performing Query.');
+			return null;
+			connection.end();
 		});
-
-		connection.end();
-
 	}
 	
-}
+	extract_data: 
+	function(req, res, next) {
+		console.log("We are in extract_data");
+		module.exports.setResHtml();
+  }
+
+ }

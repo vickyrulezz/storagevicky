a19803c67b6cae74dccaa1a5b110ebae8f42c3c4
vickyrulezz <58979571+vickyrulezz@users.noreply.github.com>
2019-12-26 13:14:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require("mysql");
 
 const http = require('http');
 
 var path = require('path');
 const VIEWS = path.join(__dirname, 'views');
-var mysql     =    require('mysql');
 
-const bodyParser = require('body-parser');
-	
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 	;
-    
-//const fetch_data_sql = require('./views/fetch_data');
+	
+// Using JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 Object.assign=require('object-assign')
 
@@ -23,9 +23,38 @@ app.set('views', VIEWS);
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//mysql configuration - Gamification project mysql connect details
+var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'custom-mysql.gamification.svc.cluster.local';
+var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
+var mysqlUser = 'xxuser'; 
+var mysqlPass = 'welcome1';
+var mysqlDb = 'sampledb';
+
+//form the connection string to connect to mysql - you can connect directly too 
+var mysqlString = 'mysql://' + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
+console.log(mysqlString);
+
+//connect to mysql/sampledb database
+var mysqlClient = mysql.createConnection(mysqlString);
+mysqlClient.connect(function (err) {
+    if (err) console.log(err);
+});
+
+//GET DB STATUS - To validate if database is running call this API ... URL/isdbon
+app.get('/api/status/db', function (req, res) {
+    mysqlClient.query('SELECT 0 + 0 AS status', function (err, rows, fields) {
+        if (err) {
+            res.send('MYSQL IS NOT CONNECTED' + JSON.stringify(err));
+        } else {
+            res.send('MYSQL IS CONNECTED - Status Msg: ' + rows[0].status);
+        }
+    });
+});
 
 
+// Getting all the pages/elements
 app.get('/', function (req, res) {
      res.render('index.html', { root : VIEWS });
 });
@@ -61,15 +90,20 @@ app.get("/background", function (req, res) {
 });
 
 // Get the data from mysql database
-app.get("/fetch_data", function (req, res) {
+//app.get("/fetch_data", function (req, res) {
   //res.sendFile('fetch_data.js', { root : VIEWS });
 
-//const fetch_data_sql = require('./views/fetch_data.js');
-//console.log(fetch_data_sql.extract_data());
-var fetch_data_sql = require('./views/test_sql.js');
-fetch_data_sql.extract_data();
-console.log("Value of resultpage is :");
-console.log(fetch_data_sql.resultpage);
+
+//GET ALL PRODUCTS - To retrieve all all products call this API ... URL/api/getproducts
+app.get('/get_all_products',(req, res) => {
+let sql = "select XXSKU.ITEM_NUMBER SKU, XXSKU.DESCRIPTION,XXPR.LIST_PRICE from XXIBM_PRODUCT_SKU XXSKU,XXIBM_PRODUCT_PRICING XXPR,XXIBM_PRODUCT_STYLE XXPS,XXIBM_PRODUCT_CATALOGUE XXPC where XXSKU.ITEM_NUMBER = XXPR.ITEM_NUMBER and XXSKU.STYLE_ITEM = XXPS.ITEM_NUMBER AND XXSKU.CATALOGUE_CATEGORY=XXPC.COMMODITY";  
+console.log(sql);
+  let query = mysqlClient.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+
 	
 //create the server for browser access
 const server = http.createServer((req, res)=>{
